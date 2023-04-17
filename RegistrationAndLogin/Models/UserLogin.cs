739fc5568c6c550c87b084e3b006080da07acad7
iodebiyi <i.o.odebiyi-2021@hull.ac.Uk>
2023-04-17 13:36:22 +0100

    Add project files.

---
+++ RegistrationAndLogin/Models/UserLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationAndLogin.Models
+{
+    public class UserLogin
+    {
+         [Display(Name ="Student ID")]
+         [Required(AllowEmptyStrings =false, ErrorMessage ="Email ID required")]
+         public string EmailID { get; set; } 
+
+      /*  [Display(Name = "Student ID")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Student ID required")]
+        public string StudentID { get; set; } */
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Password required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
